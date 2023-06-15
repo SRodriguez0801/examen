9add2af5c48f2e3ae91066b6c69f3ac5e08099f9
SRodriguez0801 <131110417+SRodriguez0801@users.noreply.github.com>
2023-06-14 20:10:50 -0600

    avance5

--- Electrodomestico/Electrodomestico/Electrodomestico/ViewModels/viewModelsResultadoCelular.cs
+++ Electrodomestico/Electrodomestico/Electrodomestico/ViewModels/viewModelsResultadoCelular.cs
@@ -1,186 +1,58 @@
-﻿using System.Collections.ObjectModel;
-using Electrodomestico.Views;
-using System;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization.Formatters.Binary;
 using Electrodomestico.Models;
 using Xamarin.Forms;
-using System.Collections.Generic;
-using System.Windows.Input;
 
 namespace Electrodomestico.ViewModels
 {
     public class ViewModelsResultadoCelular : INotifyPropertyChanged
     {
-        public List<DateTime> FechasMantenimiento { get; } = new List<DateTime>();
         private Celular resultadoCelular;
-        public ICommand AgregarCelular { get; }
+
         public ViewModelsResultadoCelular()
         {
-            //Abrir y leer el archivo 
+            listaCelulares = new ObservableCollection<Celular>();
+
+            // Cargar la lista de celulares serializados
             string ruta = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "celulares.bin");
             try
             {
                 if (File.Exists(ruta))
                 {
-                    // Abrir y leer el archivo
                     byte[] data = File.ReadAllBytes(ruta);
-                    MemoryStream memory = new MemoryStream(data);
-                    BinaryFormatter formatter = new BinaryFormatter();
-                    listaCelulares = (ObservableCollection<Models.Celular>)formatter.Deserialize(memory);
-                    memory.Close();
-                }
-                else
-                {
-                    // El archivo no existe, se crea una nueva lista vacía
-                    listaCelulares = new ObservableCollection<Models.Celular>();
-                }
-            }
-            catch (Exception ex)
-            {
-                // Manejar la excepción al abrir el archivo
-                // Aquí puedes agregar tu lógica personalizada para manejar el error
-                Console.WriteLine("Error al abrir el archivo: " + ex.Message);
-                listaCelulares = new ObservableCollection<Models.Celular>();
-            }
-            AgregarCelular = new Command(() =>
-            {
-                Models.Celular celular = new Models.Celular()
-                {
-                    Nombre = this.Nombre,
-                    Modelo = this.Modelo,
-                    FechaLanzamiento = this.FechaLanzamiento,
-                    Voltaje = this.Voltaje,
-                    PrecioCompra = this.PrecioCompra,
-                    SistemaOperativo = this.SistemaOperativo,
-                    OperadorRed = this.OperadorRed
-                };
-
-                CalcularPrecioVenta(celular);
-                listaCelulares.Add(celular);
-                AgregarMantenimiento(DateTime.Now);
-
-                ResultadoCelular = celular;
-                // Rutina para serializar la lista de celulares
-                try
-                {
-                    BinaryFormatter formatter = new BinaryFormatter();
-                    using (MemoryStream memory = new MemoryStream())
+                    using (MemoryStream memory = new MemoryStream(data))
                     {
-                        formatter.Serialize(memory, listaCelulares);
-                        byte[] serializedData = memory.ToArray();
-                        File.WriteAllBytes(ruta, serializedData);
+                        BinaryFormatter formatter = new BinaryFormatter();
+                        listaCelulares = (ObservableCollection<Celular>)formatter.Deserialize(memory);
                     }
                 }
-                catch (Exception ex)
-                {
-                    // Manejar la excepción al serializar y guardar en el archivo
-                    // Aquí puedes agregar tu lógica personalizada para manejar el error
-                    Console.WriteLine("Error al guardar en el archivo: " + ex.Message);
-                }
-
-                LimpiarCampos();
-            });
-        }
-        public void AgregarMantenimiento(DateTime fecha)
-        {
-            if (listaCelulares.Count > 0)
-            {
-                listaCelulares[listaCelulares.Count - 1].FechasMantenimiento.Add(fecha);
             }
-        }
-        public ObservableCollection<Models.Celular> listaCelulares { get; set; }
-
-        string nombre;
-        public string Nombre
-        {
-            get => nombre;
-            set
+            catch (System.Exception ex)
             {
-                nombre = value;
-                OnPropertyChanged(nameof(Nombre));
+                Console.WriteLine("Error al abrir el archivo: " + ex.Message);
             }
         }
 
-        string modelo;
-        public string Modelo
-        {
-            get => modelo;
-            set
-            {
-                modelo = value;
-                OnPropertyChanged(nameof(Modelo));
-            }
-        }
-        DateTime fechaLanzamiento;
-        public DateTime FechaLanzamiento
-        {
-            get => fechaLanzamiento;
-            set
-            {
-                fechaLanzamiento = value;
-                OnPropertyChanged(nameof(FechaLanzamiento));
-            }
-        }
-
-        string voltaje;
-        public string Voltaje
-        {
-            get => voltaje;
-            set
-            {
-                voltaje = value;
-                OnPropertyChanged(nameof(Voltaje));
-            }
-        }
-
-        decimal precioCompra;
-        public decimal PrecioCompra
-        {
-            get => precioCompra;
-            set
-            {
-                precioCompra = value;
-                OnPropertyChanged(nameof(PrecioCompra));
-            }
-        }
-        string sistemaOperativo;
-        public string SistemaOperativo
-        {
-            get => sistemaOperativo;
-            set
-            {
-                sistemaOperativo = value;
-                OnPropertyChanged(nameof(SistemaOperativo));
-            }
-        }
-
-        string operadorRed;
-        public string OperadorRed
-        {
-            get => operadorRed;
-            set
-            {
-                operadorRed = value;
-                OnPropertyChanged(nameof(OperadorRed));
-            }
-        }
-       
         public ViewModelsResultadoCelular(Celular celular)
         {
             ResultadoCelular = celular;
         }
+        Celular resultado;
         public Celular ResultadoCelular
         {
-            get { return resultadoCelular; }
+            get => resultadoCelular;
             set
             {
-                resultadoCelular = value;
-                OnPropertyChanged(nameof(ResultadoCelular));
+                resultadoCelular = value; OnPropertyChanged(nameof(ResultadoCelular));
             }
         }
+
+        public ObservableCollection<Celular> listaCelulares { get; set; } = new ObservableCollection<Celular>();
+
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -188,20 +60,7 @@ namespace Electrodomestico.ViewModels
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-        public void LimpiarCampos()
-        {
-            Nombre = string.Empty;
-            Modelo = string.Empty;
-            FechaLanzamiento = DateTime.MinValue;
-            Voltaje = string.Empty;
-            PrecioCompra = 0.0m;
-        }
-        public void CalcularPrecioVenta(Models.Celular celular)
-        {
-            celular.PrecioVenta = celular.PrecioCompra * 1.35m;
-        }
-
-
     }
 }
+
 
